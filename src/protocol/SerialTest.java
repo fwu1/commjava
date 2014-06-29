cd4bba26c2cc0e579d8de9884d801a5d50cbf9d8
Fujun Wu <fwu@altheadx.com>
2014-06-29 13:52:23 -0700

    Init

---
+++ src/protocol/SerialTest.java
@@ -0,0 +1,286 @@
+package protocol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import gnu.io.CommPortIdentifier; 
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent; 
+import gnu.io.SerialPortEventListener; 
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+
+public class SerialTest implements SerialPortEventListener {
+	SerialPort serialPort;
+        /** The port we're normally going to use. */
+	private static final String PORT_NAMES[] = { 
+//			"/dev/tty.usbserial-A9007UX1", // Mac OS X
+//                        "/dev/ttyACM0", // Raspberry Pi
+//			"/dev/ttyUSB0", // Linux
+			"COM8", // Windows
+	};
+	/**
+	* A BufferedReader which will be fed by a InputStreamReader 
+	* converting the bytes into characters 
+	* making the displayed results codepage independent
+	*/
+	private BufferedReader input;
+	/** The output stream to the port */
+	private OutputStream output;
+	/** Milliseconds to block while waiting for port open */
+	private static final int TIME_OUT = 3000;
+	/** Default bits per second for COM port. */
+	private static final int DATA_RATE = 9600;
+
+	private List <Byte> buff= new ArrayList <Byte>();
+	
+	public void initialize() {
+                // the next line is for Raspberry Pi and 
+                // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
+                System.setProperty("gnu.io.rxtx.SerialPorts", "COM8");
+
+		CommPortIdentifier portId = null;
+		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+		//First, Find an instance of serial port as set in PORT_NAMES.
+		while (portEnum.hasMoreElements()) {
+			CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+			System.out.println("Port id:"+currPortId.getName());
+			for (String portName : PORT_NAMES) {
+				if (currPortId.getName().equals(portName)) {
+					portId = currPortId;
+					break;
+				}
+			}
+		}
+		if (portId == null) {
+			System.out.println("Could not find COM port.");
+			return;
+		}
+
+		try {
+			// open serial port, and use class name for the appName.
+			serialPort = (SerialPort) portId.open(this.getClass().getName(),
+					TIME_OUT);
+
+			// set port parameters
+			serialPort.setSerialPortParams(DATA_RATE,
+					SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+
+			// open the streams
+			input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+			output = serialPort.getOutputStream();
+
+			// add event listeners
+			serialPort.addEventListener(this);
+			serialPort.notifyOnDataAvailable(true);
+		} catch (Exception e) {
+			System.err.println(e.toString());
+		}
+	}
+
+	/**
+	 * This should be called when you stop using the port.
+	 * This will prevent port locking on platforms like Linux.
+	 */
+	public synchronized void close() {
+		if (serialPort != null) {
+			serialPort.removeEventListener();
+			serialPort.close();
+		}
+	}
+
+	public  void buffClear() {
+		buff.clear();
+	}
+	
+	public  void buffWrite(byte d)
+	{
+		//System.out.printf("{%c}", (char)d);
+		buff.add(d);
+	}
+
+	public  byte buffRead()
+	{
+		byte d=buff.get(0);
+		buff.remove(0);
+		return d;
+	}
+
+	public String buffReadln(int timeout)
+	{
+		StringBuilder response= new StringBuilder();
+		int delay=50;
+		int timeoutCount=0;
+		while(timeoutCount<timeout) {
+			if(buffDataSize()>0) {
+				byte dread=buffRead();
+				//System.out.printf("[%d]\n",dread);
+				if(dread==(byte)'\r')
+					break;
+				if(dread==(byte)'\n')
+					continue;
+				else {
+					response.append((char)dread);
+				}
+			}
+			else {
+				try {
+					Thread.sleep(delay);
+					timeoutCount+=delay;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		}
+		
+		if(timeoutCount>=timeout) {
+			System.out.printf("-----------Timeout: '%s' \n",response);
+			return null;
+		}
+		return response.toString();
+		
+	}
+	
+	public  int buffDataSize()
+	{
+		return buff.size();
+	}
+	
+	/**
+	 * Handle an event on the serial port. Read the data and print it.
+	 */
+	public synchronized void serialEvent(SerialPortEvent oEvent) {
+		if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+			try {
+				//String inputLine=input.readLine();
+				//for(int i=0;i<inputLine.length();i++)
+				//	buffWrite((byte)inputLine.charAt(i));
+				
+				int c;
+				while ((c = input.read()) != -1) {
+				    //Since c is an integer, cast it to a char. If it isn't -1, it will be in the correct range of char.
+					buffWrite((byte)c);//
+					//System.out.printf("[%c]", (char)c);
+				}
+				
+				
+			} catch (Exception e) {
+				//System.out.println("ERROR");
+				//System.err.println(e.toString());
+			}
+		}
+		// Ignore all the other eventTypes, but you should consider the other ones.
+	}
+
+	public void writeln(String txt) {
+		
+		try {
+			for(int i=0;i<txt.length();i++) {
+				output.write(txt.charAt(i));
+				output.flush();
+			}
+			output.write('\r');
+			output.flush();
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public String sendCommand(String command,int timeout)
+	{
+		buffClear();
+		//System.out.printf("send commads '%s'\n",command);
+		writeln(command);
+		String line1=buffReadln(timeout);
+		if(line1!=null) {
+			//System.out.printf("line1=%s\n", line1);
+			if(!line1.equals(command)) {
+				System.out.printf("Waring, command echoed not mache:'%s'!='%s'\n", command,line1);
+			}
+			String line2=buffReadln(timeout);
+			if(line2!=null) {
+				//System.out.printf("line2=%s\n", line2);
+				return line2;
+			}
+			else 
+				System.out.printf("line2 timeout\n");
+		}
+		else
+			System.out.printf("line1 timeout\n");
+		return null;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		SerialTest task = new SerialTest();
+		task.initialize();
+		Thread t=new Thread() {
+			public void run() {
+				//the following line will keep this app alive for 1000 seconds,
+				//waiting for events to occur and responding to them (printing incoming messages to console).
+				try {
+					Thread.sleep(1000000);
+				} catch (InterruptedException ie) {}
+			}
+		};
+		t.start();
+
+		
+		if(task.output!=null) {
+			int timeout=3000;
+			int count=0;
+			byte d=0;
+			Thread.sleep(500);
+			String resp=task.sendCommand("test",500);
+			System.out.printf("resp=%s\n",resp);
+
+			
+			resp=task.sendCommand("pm a4 o",timeout);
+			System.out.printf("resp=%s\n",resp);
+
+			resp=task.sendCommand("pm a5 o",timeout);
+			System.out.printf("resp=%s\n",resp);
+
+			while (count<60) {
+				
+				Thread.sleep(100);
+				resp=task.sendCommand("dw a4 "+ ((count%2==0)?"h":"l"),timeout);
+				if(resp==null || !resp.equals("OK")) {
+					System.out.printf("resp=%s\n",resp);
+					continue;
+				}
+
+				resp=task.sendCommand("dw a5 "+ ((count%2==0)?"l":"h"),timeout);
+				if(resp==null || !resp.equals("OK")){
+					System.out.printf("resp=%s\n",resp);
+					continue;
+				}
+				//System.out.printf("resp=%s\n",resp);
+				
+				resp=task.sendCommand("ar a0",timeout);
+				if(resp==null){
+					System.out.printf("resp=%s\n",resp);
+					continue;
+				}
+				System.out.printf("******* resp of %d is '%s'\n",count,resp);
+				
+				Thread.sleep(600);
+				count++;
+			}
+		}
+		task.close();
+		System.out.println("Ended");
+	}
+}
---
+++ xml/readln/readln.ino
@@ -0,0 +1,218 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+#define BUFFSIZE 100
+char buff[BUFFSIZE+1];
+int buffCount=0;
+int count=0;
+//int wordPos=0;
+int wordEndPos=0;
+
+#define keyRETRUN		'\r'
+#define keyBACKSPACE  	127
+#define NoPin			0xFF
+
+char * getWord()
+{
+	int wordPos=-1;
+	while(wordEndPos<buffCount && buff[wordEndPos]==' ')
+		wordEndPos++;
+	if(wordEndPos<buffCount && buff[wordEndPos]!=' ')
+		wordPos=wordEndPos;
+	else
+		return NULL;
+
+	while(wordEndPos<buffCount && (buff[wordEndPos]!=' ' && buff[wordEndPos]!=0))
+		wordEndPos++;
+	if(wordEndPos>wordPos) {
+		buff[wordEndPos++]=0;
+		return buff+wordPos;
+	}
+	return NULL;
+}
+
+
+
+uint8_t getPinID(char * pinName)
+{
+	uint8_t id=NoPin;
+	char c0=pinName[0];
+	char c1=pinName[1];
+	if(c0=='a' && c1>='0' && c1<='7')  
+		return A0+(c1-'0');
+	if(c0>='0' && c0<='9') {
+		uint8_t id0=c0-'0';
+		if(c1>='0' && c1<='9') {
+			id=10*id0+(c1-'0');
+		}
+		else if (c1==0)
+			id=id0;
+	}
+	return id;
+}
+
+void process()
+{
+	char *rst="Fail";
+	buff[buffCount]=0;
+	
+	char txt[200];
+	//sprintf(txt,"P:'%s'\r\n",buff);
+	//Serial.write(txt);
+	
+	char *word;
+	wordEndPos=0;
+	
+	char * cmd=getWord();
+	//Serial.println(cmd);
+	
+	toLowcase(cmd);
+	if(strcmp(cmd,"pinmode")==0 || strcmp(cmd,"pm")==0) {
+		char * pin =getWord();
+		if(pin==NULL) 
+			rst="No pin defined";
+		else {
+			toLowcase(pin);
+			uint8_t pinId=getPinID(pin);
+			if(pinId!=NoPin) {
+				char * valueWord =getWord();
+				if(valueWord==NULL) 
+					rst="No value defined";
+				else {
+					toLowcase(valueWord);
+					uint8_t value;
+					int pass=1;
+					if(strcmp(valueWord,"output")==0 || strcmp(valueWord,"o")==0)
+						value=OUTPUT;
+					else if(strcmp(valueWord,"input")==0 || strcmp(valueWord,"i")==0)
+						value=INPUT;
+					else {
+						pass=0;
+						rst="Wrong value";
+					}
+					
+					if(pass) {
+						pinMode(pinId,value);
+						rst="OK";
+					}
+				}
+			}
+			else 
+				rst="wrong pin name";
+		}
+			
+	}
+	else if(strcmp(cmd,"digitalWrite")==0 || strcmp(cmd,"dw")==0) {
+		char * pin =getWord();
+		if(pin==NULL) 
+			rst="No pin defined";
+		else {
+			toLowcase(pin);
+			uint8_t pinId=getPinID(pin);
+			if(pinId!=NoPin) {
+				char * valueWord =getWord();
+				if(valueWord==NULL) 
+					rst="No value defined";
+				else {
+					toLowcase(valueWord);
+					uint8_t value;
+					int pass=1;
+					if(strcmp(valueWord,"high")==0 || strcmp(valueWord,"h")==0)
+						value=HIGH;
+					else if(strcmp(valueWord,"low")==0 || strcmp(valueWord,"l")==0)
+						value=LOW;
+					else {
+						pass=0;
+						rst="Wrong value";
+					}
+					
+					if(pass) {
+						digitalWrite(pinId,value);
+						rst="OK";
+					}
+				}
+			}
+			else 
+				rst="wrong pin name";
+		}
+			
+	}	
+	else if(strcmp(cmd,"analogRead")==0 || strcmp(cmd,"ar")==0) {
+		char * pin =getWord();
+		if(pin==NULL) 
+			rst="No pin defined";
+		else {
+			toLowcase(pin);
+			uint8_t pinId=getPinID(pin);
+			if(pinId!=NoPin) {
+				int value=analogRead(pinId);
+				sprintf(txt,"%d OK",value);
+				//sprintf(txt,"PASS",value);
+				rst=txt;
+			}
+			else 
+				rst="wrong pin name";
+		}
+			
+	}
+	
+	else {
+		rst="wrong command";
+	}
+	
+	Serial.write(rst);
+	Serial.write("\r\n");
+	
+/*	
+	do {
+		word=getWord();
+		if(word!=NULL)
+			Serial.println(word);
+	} while (word!=NULL);
+*/	
+	
+}
+
+void toLowcase(char* word)
+{
+	while(*word!=0) {
+		if(*word>='A' && *word<='Z')
+			*word-=32;
+		word++;
+	}
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  if (Serial.available()) {
+    /* read the most recent byte */
+    byte byteRead = Serial.read();
+	
+	// echo
+	Serial.write(byteRead);
+	if(byteRead==keyRETRUN) {
+		Serial.write('\n');
+		process();
+		buffCount=0;
+	}
+	else if(byteRead==keyBACKSPACE) {
+		// remove one character from the buffer
+		if(buffCount>0)
+			buffCount--;
+	}
+	else {
+		if(byteRead<32) {
+			//int d=byteRead;
+			//Serial.write('#');
+			//Serial.write('A'+byteRead);
+		}
+		else {
+			// save to buff
+			if(buffCount<BUFFSIZE) {
+				buff[buffCount++]=byteRead;
+		}
+		}
+	}
+  }
+}
